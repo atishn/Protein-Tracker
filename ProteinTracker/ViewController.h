ef11f4ac1c5d912fa1582604bd82d43231cce3f7
Atish Narlawar <atish.narlawar@gmail.com>
2014-08-06 07:13:07 -0400

    Initial Commit

---
+++ ProteinTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProteinTracker
+//
+//  Created by HUGE | Atish Narlawar on 8/6/14.
+//  Copyright (c) 2014 HugeInc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
