4b206f5c0f12da211d17d45250d2c09d7494308c
Atish Narlawar <atish.narlawar@gmail.com>
2014-08-08 20:20:12 -0400

    first commit

---
+++ ProteinTracker/HistoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  HistoryViewController.h
+//  ProteinTracker
+//
+//  Created by HUGE | Atish Narlawar on 8/6/14.
+//  Copyright (c) 2014 HugeInc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HistoryViewController : UIViewController
+
+@end
